e66ed485af1d1df414159e628a52c11b3034391e
JYeonJun <jyj3898@naver.com>
2022-07-31 18:21:05 +0900

    CRA

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,7 +1,11 @@
+import Button from "./Button";
+import PropTypes from "prop-types";
+
 function App() {
   return (
     <div>
       <h1>Welcome back!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
