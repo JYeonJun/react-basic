e4c6a62b55bc240456534b29a5cf00c07f1f0fbf
JYeonJun <jyj3898@naver.com>
2022-08-02 16:15:03 +0900

    Installed React-Router

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,23 +1,8 @@
 import { useState, useEffect } from "react";
-import styles from "./Label.module.css";
+import Movie from "./components/Movie";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+  return null;
 }
 
 export default App;
